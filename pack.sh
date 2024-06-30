c8079e2db593bfa09236d4e9f6f13a0065b12a90
tuhao <tuhao@weipie.com>
2024-06-30 18:48:24 +0800

    feat: 优化打包脚本

--- pack.sh
+++ pack.sh
@@ -2,8 +2,7 @@
 
 PROJECT_NAME="bdntoy"
 VERSION=v0.0.3
-RELEASE_NAME="${PROJECT_NAME}"-"${VERSION}"
-RELEASE_BODY="Release version ${VERSION}"
+RELEASE_NAME="${PROJECT_NAME}-${VERSION}"
 OUTPUT_DIR="bin" # 打包目录
 
 # 平台和架构数组
@@ -16,22 +15,29 @@ do
     for ARCH in "${ARCHITECTURES[@]}"
     do
         echo "Building for ${PLATFORM} ${ARCH}..."
-        GOOS=${PLATFORM} GOARCH=${ARCH} go build -o ${OUTPUT_DIR}/${PROJECT_NAME}_${PLATFORM}_${ARCH}
+        # 创建输出目录
+        PLATFORM_OUTPUT_DIR="${OUTPUT_DIR}/${PLATFORM}_${ARCH}"
+        mkdir -p ${PLATFORM_OUTPUT_DIR}
+
+        # windows 平台需要加后缀.exe
+        if [ ${PLATFORM} = "windows" ]; then
+            OUTPUT_NAME="${PROJECT_NAME}.exe"
+        else
+            OUTPUT_NAME="${PROJECT_NAME}"
+        fi
+
+        # 设置环境变量并构建
+        GOOS=${PLATFORM} GOARCH=${ARCH} go build -o ${PLATFORM_OUTPUT_DIR}/${OUTPUT_NAME}
+
+        # 创建压缩包
+        echo "Creating ${PLATFORM} ${ARCH} release packages..."
+        (cd "${PLATFORM_OUTPUT_DIR}" && tar -czvf "../${RELEASE_NAME}_${PLATFORM}_${ARCH}.tar.gz" ${OUTPUT_NAME})
+        (cd "${PLATFORM_OUTPUT_DIR}" && zip -r "../${RELEASE_NAME}_${PLATFORM}_${ARCH}.zip" ${OUTPUT_NAME})
     done
 done
 
-# 创建一个压缩文件
-echo "Creating release package..."
-RELEASE_TAR="${RELEASE_NAME}.tar.gz"
-tar -czvf "${RELEASE_TAR}" -C ${OUTPUT_DIR} .
-
-RELEASE_ZIP="${RELEASE_NAME}.zip"
-zip -r "${RELEASE_ZIP}" ${OUTPUT_DIR}/*
-
 # 清理临时文件
 echo "Cleaning up..."
-rm -rf ${OUTPUT_DIR}
-#rm "${RELEASE_TAR}"
-#rm "${RELEASE_ZIP}"
+#rm -rf ${OUTPUT_DIR}
 
 echo "Release process completed!"
